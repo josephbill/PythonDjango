11b5246bad7c1c6b9ff6746b597b8f3c42c31152
josephbill <josephbill00@gmail.com>
2023-03-15 16:51:07 +0300

    fix on getting json response for Web API and JSON

--- cruddjangoexample/settings.py
+++ cruddjangoexample/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'employee',
     'django_daraja',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'cruddjangoexample.urls'
 
